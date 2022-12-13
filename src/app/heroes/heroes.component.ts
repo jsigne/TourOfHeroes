a779fabe4fc1f4f025999348a66b64b3ca8f96a4
JSigne <signe.josie@gmail.com>
2022-12-13 17:28:13 +0100

    [COMPONENT][HEROES] Affichage d'une liste de Hero à partir de données test

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HEROES } from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class HeroesComponent {
 
-  hero = 'Windstorm';
+  heroes = HEROES;
 
   constructor() { }
 
