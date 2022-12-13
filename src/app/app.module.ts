a54f9f064de4de3aec30c9de26b94a21c47289c8
JSigne <signe.josie@gmail.com>
2022-12-13 17:48:36 +0100

    [COMPONENT][HERO-DETAIL] Ajout du composant hero-detail qui permettra d'afficher les détails d'un hero donné

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailsComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailsComponent
   ],
   imports: [
     BrowserModule
