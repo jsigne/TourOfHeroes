8c37c015ce1447afcb8803a02c893fbb16bb3830
JSigne <signe.josie@gmail.com>
2022-12-14 19:37:37 +0100

    [ROOTING] Affiche les details d'un hero selectionné.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { Hero } from '../hero'
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../service/hero.service';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -7,5 +11,22 @@ import { Hero } from '../hero'
 })
 export class HeroDetailsComponent{
     @Input() hero?: Hero;
+
+    constructor(
+      private route: ActivatedRoute,
+      private heroService: HeroService,
+      private location: Location
+    ) {}
+
+    ngOnInit(): void {
+      this.getHero();
+    }
+
+    getHero(): void {
+      const id = Number(this.route.snapshot.paramMap.get('id'));
+      this.heroService.getHero(id)
+        .subscribe(hero => this.hero = hero);
+    }
+
 
 }
