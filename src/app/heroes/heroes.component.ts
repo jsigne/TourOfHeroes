6a5f9095847a3e86fba3e7f1a9e9403d7f204170
JSigne <signe.josie@gmail.com>
2022-12-14 17:52:27 +0100

    [MESSAGE] Affichage d'un message envoyé par le service hero et le component heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component} from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../service/hero.service';
+import { MessageService } from '../service/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,7 +15,7 @@ export class HeroesComponent {
 
   selectedHero!: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   ngOnInit(): void {
     this.getHeroes()
@@ -22,6 +23,7 @@ export class HeroesComponent {
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
