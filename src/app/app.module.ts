a579378f08af3a6404091068cfb2a683981c998d
JSigne <signe.josie@gmail.com>
2022-12-14 18:08:57 +0100

    [APP] Ajout du routing pour naviguer entre les component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,22 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailsComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailsComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
 
-    FormsModule
+    FormsModule,
+     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
