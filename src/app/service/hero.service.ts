4c6903954eaccb2cd06eee4dbbcbe4f3f8478dc8
JSigne <signe.josie@gmail.com>
2022-12-19 14:38:50 +0100

    Gestion des erreurs d'acces aux données du serveur

--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -26,19 +26,53 @@ export class HeroService {
   }
   */
 
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
-
+ /*
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find(h => h.id === id)!;
     this.log(`fetched hero id=${id}`);
     return of(hero);
   }
+ */
+  getHero(id: number): Observable<Hero> {
+    return this.http.get<Hero>(`${this.heroesUrl}/${id}`)
+          .pipe(
+            tap(_ => this.log('fetched hero id=${id}')),
+            catchError(this.handleError<Hero>('getHero id=${id}'))
+          );
+  }
 
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
 }
